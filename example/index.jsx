a3841a89ef58e4d22c3bacdb344c29db7f4f6613
Margox <margox@foxmail.com>
2019-07-29 22:41:12 +0800

    v0.0.21

--- example/index.jsx
+++ example/index.jsx
@@ -1,29 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import BraftFinder, { ImageUtils } from '../src'
+import React from "react";
+import ReactDOM from "react-dom";
+import BraftFinder, { ImageUtils } from "../src";
 
 class Demo extends React.Component {
-
-  constructor (props) {
-
-    super(props)
+  constructor(props) {
+    super(props);
     this.braftFinder = new BraftFinder({
-      language: 'pl'
-    })
-
+      language: "pl"
+    });
   }
 
-  beforeRemove = (items) => {
-    return confirm('确认删除这些项目?')
-  }
+  beforeRemove = items => {
+    return confirm("确认删除这些项目?");
+  };
 
-  handelFileSelect = (files) => {
-    return [].slice.call(files, 0, 3)
-  }
+  handelFileSelect = files => {
+    return [].slice.call(files, 0, 3);
+  };
 
   render() {
-
-    const FinderComponent = this.braftFinder.ReactComponent
+    const FinderComponent = this.braftFinder.ReactComponent;
 
     return (
       <div className="demo">
@@ -32,21 +28,20 @@ class Demo extends React.Component {
             audio: false,
             video: false
           }}
-          onSelect={item => console.log('seleced:', item)}
-          onBeforeSelect={item => console.log('will select:', item)}
-          onDeselect={item => console.log('deselected:', item)}
-          onBeforeDeselect={item => console.log('will deselect:', item)}
-          onInsert={items => console.log('insert:', items)}
-          onBeforeInsert={items => console.log('will insert:', items)}
-          onRemove={items => console.log('removed', items)}
+          language="ru"
+          onSelect={item => console.log("seleced:", item)}
+          onBeforeSelect={item => console.log("will select:", item)}
+          onDeselect={item => console.log("deselected:", item)}
+          onBeforeDeselect={item => console.log("will deselect:", item)}
+          onInsert={items => console.log("insert:", items)}
+          onBeforeInsert={items => console.log("will insert:", items)}
+          onRemove={items => console.log("removed", items)}
           onBeforeRemove={this.beforeRemove}
           onFileSelect={this.handelFileSelect}
         />
       </div>
-    )
-
+    );
   }
-
 }
 
-ReactDOM.render(<Demo />, document.querySelector('#root'))
+ReactDOM.render(<Demo />, document.querySelector("#root"));
